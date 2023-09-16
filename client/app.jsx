c75ab4f9ba168e066a419d2eced6652da5f02565
Shi Kuang <skuang125@gmail.com>
2023-09-16 15:55:20 -0700

    updated app

--- client/app.jsx
+++ client/app.jsx
@@ -6,6 +6,7 @@ import Navigation from './components/navigation'
 import Results from './components/results'
 import Favorites from './components/favorites'
 import Search from './components/search'
+import Details from './components/details'
 
 
 const App = () => {
@@ -14,10 +15,11 @@ const App = () => {
 
       <Routes>
         <Route path='/search' element={<Search />} />
-        <Route path='/results' element={<Result />} />
+        <Route path='/results' element={<Results />} />
         <Route path='/details' element={<Details />} />
-        <Route path='/sellItem' element={<CreateSellPosting />} />
-        <Route path='/' element={<HomePage />} />
+        <Route path='/favorites' element={<Favorites />} />
+        {/* <Route path='/sellItem' element={<CreateSellPosting />} /> */}
+        <Route path='/' element={<Navigation />} />
       </Routes>
     </>
   );
