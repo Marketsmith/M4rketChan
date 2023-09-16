8f78c63a2e50e698eb8cdde3670dde787bcfaabe
kelvinmirhan <kelvinmirhan@gmail.com>
2023-09-16 15:52:42 -0700

    small changes

--- client/components/search.jsx
+++ client/components/search.jsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import styled from "styled-components";
+
+const Container = styled.div `display: flex; flex-direction: row;`
 
 const Search = () => {
-
-    const handleclick = async (e) => {
-
-
-    }
 
     return (
         <>
         <Navigation/>
-
-         <Link to='/results' >
-                <button type='button'> Search </button>
+        <Container>
+        <Link to='/results'>
+            <button type='button'> Search </button>
         </Link>
-        <br/>
-
+        </Container>
         </>
     );
 };
