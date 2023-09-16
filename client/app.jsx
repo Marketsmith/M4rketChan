4f71ffd5923837a9475e4247c047803755c0e7c3
Andrew Byun <andrewbyun1@gmail.com>
2023-09-16 15:07:13 -0700

    updated landing page and sell post creation page

--- client/app.jsx
+++ client/app.jsx
@@ -3,16 +3,16 @@ import { Routes, Route } from 'react-router-dom';
 import './stylesheets/styles.css';
 
 import search from './search'
+import SellItem from './components/landingpage';
 const App = () => {
   return (
     <>
-
       <Routes>
         <Route path='/search' element={<Search />} />
         <Route path='/results' element={<Result />} />
         <Route path='/details' element={<Details />} />
-        <Route path='/sellItem' element={<Details />} />
-        <Route path='/buyItem' element={<Details />} />
+        <Route path='/sellItem' element={<CreateSellPosting />} />
+        <Route path='/' element={<HomePage />} />
       </Routes>
     </>
   );
