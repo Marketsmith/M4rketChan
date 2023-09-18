bd777a86d2b892bddcdc55902cff0d0647b0268f
Connor Keyes <connorkeyes@Connors-MacBook-Air.local>
2023-09-18 10:07:11 -0700

    cloudinary api image upload

---
+++ client/components/uploadParent.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+
+const uploadParent = () => {
+
+  // const uploadWidget = () => {
+  //   const myWidget = cloudinary.createUploadWidget(
+  //     {
+  //       cloudName: cloud_name,
+  //       uploadPreset: upload_preset,
+  //       cropping: true,
+  //       multiple: false, 
+  //       context: {alt: "user_uploaded"},
+  //       clientAllowedFormats: ["images"],
+  //       maxImageWidth: 2000,
+  //     },
+  //     (error, result) => {
+  //       if (!error && result && result.event === "success") {
+  //         console.log("Done! Here is the image info: ", result.info);
+  //         document
+  //           .getElementById("uploadedimage")
+  //           .setAttribute("src", result.info.secure_url);
+  //       }
+  //     }
+  //   );
+  // };
+  
+  // document.getElementById("upload_widget").addEventListener(
+  //   "click",
+  //   function () {
+  //     console.log("clicked");
+  //     myWidget.open();
+  //   },
+  //   false
+  // );
+
+
+  return (
+    <>
+    </>
+  );
+
+};
+
+
--- client/constants/actionTypes.js
+++ client/constants/actionTypes.js
@@ -1,3 +1,10 @@
 export const SELL_BUTTON = "SELL_BUTTON";
 
 export const BUY_BUTTON = "BUY_BUTTON";
+
+export const CLOUDINARY_CONSTS = {
+  api_key: "374659647111771",
+  cloud_name: "dlxfkrk48",
+  api_secret: "d_3n7CUCzkxNfibszUer0UrxR2Y",
+  upload_preset: "j0ez2cmn"
+}
