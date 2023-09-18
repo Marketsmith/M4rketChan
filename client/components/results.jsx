96eb86120298e302ce58fd601b9688b609bc5386
Shi Kuang <skuang125@gmail.com>
2023-09-18 10:45:17 -0700

    new pull

--- client/components/results.jsx
+++ client/components/results.jsx
@@ -1,24 +1,14 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import Navigation from './navigation'
+import React from "react";
+import { Link } from "react-router-dom";
+import Navigation from "./navigation";
 
 const Results = () => {
-
-    return (
-        <>
-
-        <Navigation/>
-        {/* <Link to='/results'>
-    <button type='button'> Search </button>
-</Link> */}
-        <Searchbar/>
-
-
-        <div>HELLO THIS IS RESULTS</div>
-        <br/>
-
-        </>
-    );
+  return (
+    <>
+      <div>HELLO THIS IS RESULTS</div>
+      <br />
+    </>
+  );
 };
 
 export default Results;
