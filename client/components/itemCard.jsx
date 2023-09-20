24781d71f207e356be5d1fb7f30ae8b1ceb8dd26
Jose Velasco <josevelasco1022@gmail.com>
2023-09-20 09:58:13 -0700

    refactored itemCard component

--- client/components/itemCard.jsx
+++ client/components/itemCard.jsx
@@ -6,55 +6,38 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
-
-//styles
 import './styles.css'
-import { useContext } from "react";
-import {itemCartContext} from './searchbar'
-// Navigate
 import { useNavigate } from "react-router-dom";
 
 
-export default function ItemCard() {
-
-  // use context of items DATA 
-  const itemCart = useContext(itemCartContext);
-
-  // Navigate
+export default function ItemCard(props) {
   const navigate = useNavigate();
-
-  // itemsCart => chat : [] => useEffect hook => GET request 
 
   const messageRedirect = () => {
     navigate('../details')
   }
 
-
   return (
-    itemCart.map((item, index) => (
-      <Card key={index} sx={{ maxWidth: 345 }}>
-        <CardMedia
-          sx={{ height: 200 }}
-          image={item.picture} 
-          title="item image"
-        />
-        <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-            {item.name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {item.description}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            ${item.price}
-          </Typography>
-        </CardContent>
-        <CardActions>
-
-          <Button size="small" onClick={() => messageRedirect()}>Messages</Button>
-        </CardActions>
-      </Card>
-      
-    ))
+    <Card sx={{ maxWidth: 345 }}>
+    <CardMedia
+      sx={{ height: 200 }}
+      image={props.picture} 
+      title="item image"
+    />
+    <CardContent>
+    <Typography gutterBottom variant="h5" component="div">
+        {props.name}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        {props.description}
+      </Typography>
+      <Typography variant="body2" color="text.secondary">
+        ${props.price}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small" onClick={() => messageRedirect()}>Messages</Button>
+    </CardActions>
+  </Card>
   );
 }
